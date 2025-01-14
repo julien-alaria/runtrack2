bb45ee0af49c0f9be11158b4ec7e84a59d7c7554
julien-alr <julienalaria@gmail.com>
2025-01-14 19:16:23 +0100

    first commit job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,1 +1,48 @@
+<?php
+// Déclaration des variables
+$bool = true;               
+$integer = 42;              
+$string = "Hello LaPlateforme!";  
+$float = 3.14;              
+
+// Début du tableau HTML
+echo "<table border='1' style='border-collapse: collapse; width: 50%; text-align: left;'>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>Type</th>";
+echo "<th>Nom</th>";
+echo "<th>Valeur</th>";
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
+
+// Ligne pour chaque variable
+echo "<tr>";
+echo "<td>boolean</td>";
+echo "<td>booleen</td>";
+echo "<td>" . ($bool ? "true" : "false") . "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>integer</td>";
+echo "<td>nombre_entier</td>";
+echo "<td>$integer</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>string</td>";
+echo "<td>chaine_de_caractere</td>";
+echo "<td>$string</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>float</td>";
+echo "<td>nombre_a_virgule</td>";
+echo "<td>$float</td>";
+echo "</tr>";
+
+// Fin du tableau HTML
+echo "</tbody>";
+echo "</table>";
+?>
 
