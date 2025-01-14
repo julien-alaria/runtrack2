ef96b04dda0df22eacd8ef19a48f60009beaae7d
julien-alr <julienalaria@gmail.com>
2025-01-14 18:54:21 +0100

    first commit

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,1 +1,18 @@
+<?php
+$str = "laPlateforme";
+echo $str;
 
+$str2 = "Vive";
+$str3 = "!";
+echo $str2. $str. $str3;
+
+$val = 6;
+echo $val;
+$val = 6 + 4;
+echo $val;
+
+$mybool = True;
+echo $mybool;
+
+$mybool = False;
+echo $mybool;
