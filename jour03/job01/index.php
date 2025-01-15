c9e0a8a93c84eba2a98386c8309dcf82673ce678
julien-alr <julienalaria@gmail.com>
2025-01-15 14:47:39 +0100

    first commit

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,1 +1,16 @@
 
+<?php
+
+$job01 = [200, 204, 173, 98, 171, 404, 459];
+
+echo $job01[3]."<br>"; 
+
+foreach ($job01 as $value) {
+    
+    if ($value %2 == 0){
+        echo $value." est pair<br>";
+    }else {
+        echo $value. " est impair<br>";
+    }
+
+}
