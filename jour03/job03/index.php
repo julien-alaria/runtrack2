d70ae0269a39d7eeccd3735cea7ae581997538fd
julien-alr <julienalaria@gmail.com>
2025-01-15 14:49:20 +0100

    first commit

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,1 +1,22 @@
+<?php
+
+$str = "I'm sorry Dave i'm afraid i can't do that";
+
+$i = 0;
+
+while (isset($str[$i])){
+    $value = $str[$i];
+     
+    if ($value == "a" || $value == "e" || $value == "i" || $value == "o" || $value == "u" || $value == "y" ||
+        $value == "A" || $value == "E" || $value == "I" || $value == "O" || $value == "U" || $value == "Y")
+        {
+            echo $value ."<br>";
+        }
+
+        $i++;
+
+}
+    
+
+
 
