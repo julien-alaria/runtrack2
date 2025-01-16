ede800c9cc66b4f217cb5937d7b3467e487c2cf9
julien-alr <julienalaria@gmail.com>
2025-01-16 12:56:52 +0100

    second commit job01

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,1 +1,32 @@
+<?php
+if (isset($_GET)){
+    var_dump($_GET);
+    }
+    
+    $i = 0;
+
+    foreach($_GET as $key => $value){
+        $i++;
+    }
+    echo $i+1
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="$_GET">
+        <input type="text" name="nom"><br>
+        <input type="text" name="prenom"><br>
+        <input type="text" name="age"><br>
+        <input type="text" name="adresse"><br>
+        <input type="submit" value="submit"><br>
+    </form>
+</body>
+</html>
 
