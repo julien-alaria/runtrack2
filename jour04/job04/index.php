15c0037ecb64d2da4b1af50701f27c6a44cd5cff
julien-alr <julienalaria@gmail.com>
2025-01-17 10:23:32 +0100

    first commit job04

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -1,1 +1,37 @@
+<?php
 
+    
+
+    foreach($_POST as $key => $value){
+        echo    "<table>";
+        echo    "<tr>";
+        echo    "<td>".$key."</td>";
+        echo    "<td>".$value."</td>";
+        echo    "</tr>";
+        echo    "</table>";
+    }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form action="index.php" method="post">
+        <input type="text" name="nom"><br>
+        <input type="text" name="prenom"><br>
+        <input type="submit" value="submit"><br>
+    </form>
+
+   
+
+
+</body>
+</html>
