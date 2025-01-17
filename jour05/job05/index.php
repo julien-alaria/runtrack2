501942144664e06d2450bf921079763249fd5191
julien-alr <julienalaria@gmail.com>
2025-01-17 12:35:32 +0100

    second commit job05

--- jour05/job05/index.php
+++ jour05/job05/index.php
@@ -1,1 +1,18 @@
+<?php
+
+function occurence($str, $car){
+    $j = 0;
+    for($i = 0; isset($str[$i]); $i++){
+        if($str[$i] == $car){
+            $j++;
+        }
+    }
+  return $j;
+}
+
+$str = "Bonjour";
+$car = "o";
+
+echo occurence($str, $car);
+?>
 
