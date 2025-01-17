9037692130ab9454cc3d3bfcd96177f937c26e57
julien-alr <julienalaria@gmail.com>
2025-01-17 10:21:38 +0100

    first commit job02

--- jour04/job02/index.php
+++ jour04/job02/index.php
@@ -1,1 +1,40 @@
+<?php
+if (isset($_GET)){
+    var_dump($_GET);
+    }
+    
+
+    foreach($_GET as $key => $value){
+        echo    "<table>";
+        echo    "<tr>";
+        echo    "<td>".$key."</td>";
+        echo    "<td>".$value."</td>";
+        echo    "</tr>";
+        echo    "</table>";
+    }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form action="index.php" method="$_GET">
+        <input type="text" name="nom"><br>
+        <input type="text" name="prenom"><br>
+        <input type="submit" value="submit"><br>
+    </form>
+
+   
+
+
+</body>
+</html>
 
