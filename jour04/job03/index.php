75d064c5b14c9ed94bf37a60166f56008d44d19c
julien-alr <julienalaria@gmail.com>
2025-01-17 10:22:25 +0100

    first commit job03

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,1 +1,30 @@
+<?php
+
+$i = 0;
+
+foreach($_GET as $key => $value){
+    $i++;
+}
+echo "nombre d'arguments: ".$i
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="$_POST">
+        <input type="text" name="nom"><br>
+        <input type="text" name="prenom"><br>
+        <input type="text" name="age"><br>
+        <input type="text" name="adresse"><br>
+        <input type="submit" value="submit"><br>
+    </form>
+</body>
+</html>
 
