9d41124795e371a13e8028b02f7e328e4961cad0
julien-alr <julienalaria@gmail.com>
2025-01-17 10:24:55 +0100

    first commit job06

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job06</title>
+</head>
 
+<body>
+
+    <form action="" method="get">
+
+        <input type="text" name="nombre" placeholder="Entrez un nombre:"><br><br>
+        <button type="submit">Envoyer</button>
+
+    </form>
+
+    <?php
+    
+    if (!empty($_GET['nombre'])) {
+        if ($_GET['nombre'] % 2 == 0) {
+            echo "nombre pair";
+        } else {
+            echo "nombre impair";
+        }
+    }
+    ?>
+</body>
+</html>
