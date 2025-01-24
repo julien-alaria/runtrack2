ab03f07fa776eb20a0039f24a2cb00ff6c3032b3
julien-alr <julienalaria@gmail.com>
2025-01-24 14:40:44 +0100

    first commit

--- jour09/job01/index.php
+++ jour09/job01/index.php
@@ -13,11 +13,11 @@ var_dump($pdo);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>job01</title>
 </head>
 <style>
     table{
